1d29f0e2609fd8dfe9d84e91bc0e8bdbccef937f
John Feng <johnfengphd@gmail.com>
2024-04-17 14:59:11 -0700

    imported files from Pockels-Automation-Software

---
+++ Txt_to_Image.py
@@ -0,0 +1,130 @@
+import streamlit as st
+import numpy as np
+import plotly.express as px
+import pandas as pd
+
+st.set_page_config(
+    page_title="Txt to Image",
+    page_icon="🧊",)
+
+@st.cache_data  # maks image update faster
+def txt2array(txt_file: str) -> np.ndarray:
+    """Used in the store_in_hdf5 method.
+    Convert a .txt file to a 2D numpy array."""
+    data = np.loadtxt(txt_file, skiprows=19)
+
+    # Step 1: Determine the dimensions of the image
+    max_row = int(np.max(data[:, 0])) + 1  # Adding 1 because index starts at 0
+    max_col = int(np.max(data[:, 1])) + 1  # Adding 1 because index starts at 0
+
+    # Step 2: Create an empty 2D array
+    image_array = np.zeros((max_row, max_col), dtype=np.uint16)
+
+    # Step 3: Fill the array with pixel values
+    for row in data:
+        r, c, val = int(row[0]), int(row[1]), row[2]
+        image_array[r, c] = val + 1
+
+    return image_array
+
+
+def plot_image(image_array: np.ndarray) -> None:
+    """Plot a 2D numpy array as an image."""
+    figure = px.imshow(image_array)
+
+    return figure
+
+
+
+st.title("Pockels image viewer")
+
+color_theme = st.sidebar.radio(
+    "Choose a color theme:", ("Viridis", "Plasma", "Inferno", "Jet"), index=0
+)
+
+reverse_color_theme = st.sidebar.checkbox("Reverse color theme")
+
+if reverse_color_theme:
+    color_theme = color_theme + "_r"
+
+uploaded_file = st.sidebar.file_uploader("Please upload a .txt file: ", type=["txt"], key="file1")
+uploaded_file_2 = st.sidebar.file_uploader("Please upload a .txt file: ", type=["txt"], key="file2")
+
+
+# checks if file is uploaded
+if uploaded_file is not None:
+    st.subheader("Image 1:")
+
+    # convert bytes to a numpy array
+    image_array_1 = txt2array(uploaded_file).astype(int)
+
+    # making float min/max values of the slider based on min/max values of image array
+    min_val, max_val = st.slider(
+        label="Color range slider:",
+        min_value=0,
+        max_value=int(image_array_1.max()),
+        value=[0, 700],
+        step=10,
+    )
+
+    figure = px.imshow(
+        image_array_1, color_continuous_scale=color_theme, range_color=[min_val, max_val]
+    )
+
+    st.plotly_chart(figure)
+
+
+# checks if file is uploaded
+if uploaded_file_2 is not None:
+    st.subheader("Image 2:")
+
+    # convert bytes to a numpy array
+    image_array_2 = txt2array(uploaded_file_2).astype(int)
+
+    # making float min/max values of the slider based on min/max values of image array
+    min_val, max_val = st.slider(
+        label="Color range slider:",
+        min_value=0,
+        max_value=int(image_array_2.max()),
+        value=[0, 700],
+        step=10,
+    )
+
+    figure2 = px.imshow(
+        image_array_2, color_continuous_scale=color_theme, range_color=[min_val, max_val]
+    )
+
+    st.plotly_chart(figure2)
+
+
+if uploaded_file is not None and uploaded_file_2 is not None:
+
+    st.subheader("Image 1 - Image 2:")
+
+    difference_image = image_array_1 - image_array_2
+
+    # convert difference image to csv
+    data = pd.DataFrame(difference_image)
+    csv = data.to_csv(index=False).encode('utf-8')
+
+    # download button for the difference image
+    st.download_button(
+        label="Download difference image",
+        data=csv,
+        file_name="difference_image.csv",
+        mime="text/csv",
+    )
+
+    # making float min/max values of the slider based on min/max values of image array
+    min_val, max_val = st.slider(
+        label="Color range slider:",
+        min_value=int(difference_image.min()),
+        max_value=int(difference_image.max()),
+        value=[0, 700],
+        step=10,
+    )
+
+    figure3 = px.imshow(
+        difference_image, color_continuous_scale=color_theme, range_color=[min_val, max_val])
+
+    st.plotly_chart(figure3)
---
+++ pages/Polarizer_Calibration.py
@@ -0,0 +1,139 @@
+import streamlit as st
+import numpy as np
+import plotly.graph_objects as go
+from malus_law_fit import (
+    fit_malus_law,
+    malus_law,
+    radians_to_degrees,
+    get_crossed_angle,
+    get_parallel_angle,
+)
+
+st.set_page_config(
+    page_title="Polarizer Calibration",
+    page_icon=":bar_chart:",
+)
+
+
+st.title("Polarizer Calibration")
+with st.expander("How to use: "):
+    st.write("Instructions coming soon...")
+
+with st.sidebar:
+    st.header("-Fit parameters-")
+
+# Get user input, give default values as an example
+x_data = st.text_input(
+    label="Enter angles in degrees (comma-separated):",
+    value="0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160",
+)
+y_data = st.text_input(
+    label="Enter intensity data (comma-separated):",
+    value="0.75, 0.42, 0.34, 0.2, 0.24, 0.3, 0.68, 0.68, 0.87, 1.03, 1.3, 1.22, 1.33, 1.29, 0.97, 0.91, 0.84",
+)
+
+if not x_data or not y_data:
+    st.warning("Please enter some data.")
+
+if len(x_data.split(",")) != len(y_data.split(",")):
+    st.warning(f"len(x) = {len(x_data.split(','))}")
+    st.warning(f"len(y) = {len(y_data.split(','))}")
+    st.warning("X and Y data must have the same number of elements.")
+
+# Convert input to lists of floats
+x_values = [float(x.strip()) for x in x_data.split(",")]
+y_values = [float(y.strip()) for y in y_data.split(",")]
+x_values = np.array(x_values)  # Convert to numpy array
+y_values = np.array(y_values)
+
+length_condition = len(x_values) == len(y_values)
+none_condition = x_values is not None and y_values is not None
+active_conditions = length_condition and none_condition
+
+# initialize figure plot
+fig = go.Figure()
+fig.update_layout(
+    xaxis_title="angle (degrees)", yaxis_title="Intensity", showlegend=False
+)
+st.session_state.fig = fig
+
+def show_raw_data():
+
+    # Plot the data
+    fig.add_trace(
+        go.Scatter(
+            x=x_values,
+            y=y_values,
+            mode="markers+lines",
+            line=dict(color="orange", dash="dash"),
+        )
+    )
+    fig.update_layout(
+        showlegend=False,
+    )
+    st.session_state.fig = fig
+    # st.plotly_chart(fig)
+
+def do_curve_fit():
+    popt, pcov = fit_malus_law(x_values, y_values)
+    phi_deg = radians_to_degrees(popt[1])
+
+    parallel_angle = get_parallel_angle(phi_deg)
+    crossed_angle = get_crossed_angle(phi_deg)
+    st.sidebar.write(f"**I0_fit:** {popt[0]:.2f}")
+    st.sidebar.write(f"**phi_fit:** {phi_deg:.2f} degrees")
+    st.sidebar.write(f"**offset_fit:** {popt[2]:.2f}")
+    st.sidebar.write(
+        f"**parallel_angle:** {parallel_angle:.2f}"
+    )  # Display in sidebar
+    st.sidebar.write(f"**crossed_angle:** {crossed_angle:.2f}")
+
+    x_fit = np.arange(x_values[0], x_values[-1], 1)
+    y_fit = malus_law(x_fit, *popt)
+
+    # Plot the data
+    fig.update_layout(
+        showlegend=True,  # Add legend
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=x_fit,
+            y=y_fit,
+            mode="lines",
+            name="curve fit",  # Add label for curve fit
+        )
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=x_values,
+            y=y_values,
+            mode="markers+lines",
+            line=dict(color="orange", dash="dash"),
+            name="raw data",  # Add label for raw data
+        )
+    )
+    st.session_state.fig = fig
+
+def write_hello():
+    st.write("Hello")
+
+def write_bye():
+    st.write("Good bye")
+
+col1, col2, _, _ = st.columns(4)
+
+with col1:
+    if st.button(
+        "Show Raw Data", disabled=not active_conditions, key="raw_data_button"
+    ):
+        show_raw_data()
+with col2:
+    if st.button(
+        "Fit Malus Law", disabled=not active_conditions, key="curve_fit_button"
+    ):
+        do_curve_fit()
+
+container = st.container(border=True)
+# container.pyplot(st.session_state.fig)
+container.plotly_chart(st.session_state.fig)
+
